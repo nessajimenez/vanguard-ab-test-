947b24658062faf22e7c73483be383c8cd1a1585
nessajimenez <168484714+nessajimenez@users.noreply.github.com>
2024-05-31 10:05:54 -0400

    added more data to client analysis

--- Completion rate.sql
+++ Completion rate.sql
@@ -32,34 +32,46 @@ ON group_id.client_id = interactions.client_id
 WHERE interactions.process_step = 'confirm' AND group_id.variation = 'Test'
 GROUP BY clients.client_id;
 
--- 26679 people in the test group started the process
+-- 8281 people in the test did not complete the process
 SELECT clients.client_id, COUNT(process_step)
 FROM clients
 JOIN interactions
 ON clients.client_id = interactions.client_id
 JOIN group_id
 ON group_id.client_id = interactions.client_id
-WHERE interactions.process_step = 'start' AND group_id.variation = 'Test'
+WHERE group_id.variation = 'Test' AND group_id.client_id NOT IN (SELECT clients.client_id
+																FROM clients
+																JOIN interactions
+																ON clients.client_id = interactions.client_id
+																JOIN group_id
+																ON group_id.client_id = interactions.client_id
+																WHERE interactions.process_step = 'confirm' AND group_id.variation = 'Test')
 GROUP BY clients.client_id;
 
--- 23397 started in the control group
+-- 15434 people in the control group completed the process
 SELECT clients.client_id,  COUNT(process_step)
 FROM clients
 JOIN interactions
 ON clients.client_id = interactions.client_id
 JOIN group_id
 ON group_id.client_id = interactions.client_id
-WHERE interactions.process_step = 'start' AND group_id.variation != 'Test'
+WHERE interactions.process_step = 'confirm' AND group_id.variation = 'Control'
 GROUP BY clients.client_id;
 
--- 23496 completed in the control group
+-- 8098 people in the control group did not complete the process
 SELECT clients.client_id, COUNT(process_step)
 FROM clients
 JOIN interactions
 ON clients.client_id = interactions.client_id
 JOIN group_id
 ON group_id.client_id = interactions.client_id
-WHERE interactions.process_step != 'confirm' AND group_id.variation != 'Test'
+WHERE group_id.variation = 'Control' AND clients.client_id NOT IN (SELECT clients.client_id
+																FROM clients
+																JOIN interactions
+																ON clients.client_id = interactions.client_id
+																JOIN group_id
+																ON group_id.client_id = interactions.client_id
+																WHERE interactions.process_step = 'confirm' AND group_id.variation = 'Control')
 GROUP BY clients.client_id;
 
 -- 26,968 were IN the test group
@@ -69,12 +81,12 @@ JOIN group_id
 ON group_id.client_id = clients.client_id
 WHERE group_id.variation = 'Test';
 
--- 23532 were NOT in the test group
+-- 23532 were in the control group
 SELECT COUNT(clients.client_id)
 FROM clients
 JOIN group_id
 ON group_id.client_id = clients.client_id
-WHERE group_id.variation != 'Test';
+WHERE group_id.variation = 'Control';
 
 -- 20109 were in neither group
 SELECT * 
@@ -88,51 +100,93 @@ DELETE FROM group_id WHERE variation IS NULL;
 ALTER TABLE interactions
 MODIFY date_time DATETIME;
 
-SELECT * 
-FROM clients
-JOIN group_id;
-
+-- all data on those in the test group 
 SELECT *
 FROM clients
 JOIN interactions
 ON clients.client_id = interactions.client_id
 JOIN group_id
 ON group_id.client_id = interactions.client_id
-WHERE interactions.process_step != 'confirm' AND group_id.variation != 'Test';
+WHERE group_id.variation = 'Test';
 
-
-
-WITH StepDurations AS (
-    SELECT 
-        interactions.user_id,
-        interactions.process_step AS current_step,
-        MIN(b.step_datetime) AS next_step_time,
-        TIMESTAMPDIFF(SECOND, a.step_datetime, MIN(b.step_datetime)) AS duration_seconds
-    FROM 
-        user_steps a
-    LEFT JOIN 
-        user_steps b
-    ON 
-        a.user_id = b.user_id
-        AND a.step_datetime < b.step_datetime
-    GROUP BY 
-        a.user_id, a.step_name, a.step_datetime
-)
-
-SELECT 
-    current_step,
-    AVG(duration_seconds) AS average_duration_seconds
-FROM 
-    StepDurations
-GROUP BY 
-    current_step
-ORDER BY 
-    current_step;
-    
-    
--- How far each person got in the process    
-SELECT clients.client_id,clients.clnt_age, clients.gendr, COUNT(distinct(interactions.process_step)) AS count_of_steps
+-- all data on those in the control group 
+SELECT *
 FROM clients
 JOIN interactions
 ON clients.client_id = interactions.client_id
-GROUP BY clients.client_id, clients.clnt_age, clients.gendr;
+JOIN group_id
+ON group_id.client_id = interactions.client_id
+WHERE group_id.variation = 'Control';
+
+
+-- WITH StepDurations AS (
+--     SELECT 
+--         a.client_id,
+--         b.process_step AS current_step,
+--         MIN(b.date_time) AS next_step_time,
+--         TIMESTAMPDIFF(SECOND, a.date_time, MIN(b.date_time)) AS duration_seconds
+--     FROM 
+--         interactions a
+--     LEFT JOIN 
+--         interactions b
+--     ON 
+--         a.client_id = b.client_id
+--         AND a.date_time < b.date_time
+--     GROUP BY 
+--         a.client_id, b.process_step, a.date_time
+-- )
+
+-- SELECT 
+--     current_step,
+--     AVG(duration_seconds) AS average_duration_seconds
+-- FROM 
+--     StepDurations
+-- GROUP BY 
+--     current_step
+-- ORDER BY 
+--     current_step;
+    
+    
+-- How far each person got in the process    
+SELECT clients.client_id,
+	clients.clnt_age, 
+    clients.gendr, 
+    COUNT(distinct(interactions.process_step)) AS count_of_steps,
+    group_id.variation
+FROM clients
+JOIN interactions
+ON clients.client_id = interactions.client_id
+INNER JOIN group_id
+ON group_id.client_id = clients.client_id
+GROUP BY clients.client_id, clients.clnt_age, clients.gendr,group_id.variation;
+
+-- how many of each step, even if they repeated
+SELECT clients.client_id,
+	clients.clnt_age, 
+    clients.gendr, 
+    COUNT(interactions.process_step) AS count_of_steps,
+    group_id.variation
+FROM clients
+JOIN interactions
+ON clients.client_id = interactions.client_id
+INNER JOIN group_id
+ON group_id.client_id = clients.client_id
+GROUP BY clients.client_id, clients.clnt_age, clients.gendr,group_id.variation
+ORDER BY count_of_steps desc;
+
+-- how many steps even if they repeated, by group they were in
+SELECT clients.client_id, 
+	interactions.visit_id,
+	clients.clnt_age, 
+    clients.gendr, 
+    group_id.variation,
+	COUNT(interactions.process_step) AS count_of_steps
+FROM clients
+JOIN interactions
+ON clients.client_id = interactions.client_id
+INNER JOIN group_id
+ON group_id.client_id = clients.client_id
+GROUP BY clients.client_id, interactions.visit_id,
+	clients.clnt_age, 
+    clients.gendr, 
+    group_id.variation ;
